66872d3d4eddf15c06f0ed83dba9aada88971ae4
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-05-24 14:45:58 +0900

    기초 1059~1062번 추가

---
+++ Basic-100/1059.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	cout << ~n;
+
+	return 0;
+
+}
---
+++ Basic-100/1061.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n, m;
+
+	cin >> n >> m;
+
+	cout << (n & m);
+
+	return 0;
+
+}
